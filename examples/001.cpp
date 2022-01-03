a95901c55d2869485ca34c7d5521f5e210433fb6
maks <makuta.maks.mmc@gmail.com>
2022-01-03 17:27:41 +0200

    [1.2.1] remove unused files | upgrade

--- examples/001.cpp
+++ examples/001.cpp
@@ -1,34 +1,65 @@
-#include "lib/processing.h"
+//#define USE_KEY
+#include <processing.h>
 
-int u = 60;
-bool showGrid = true;
+float speed;
 
-void drawGrid(){
-    strokeWeight(1);
-    noFill();
-    stroke(200);
-    for(int col = 0;col < 9;col++)
-        line(col * u,0,col * u, width);
-    for(int row = 0;row < 9;row++)
-        line(0,row * u,height, row * u);
+class Star{
+  private:
+    float x,y,z,pz;
+  public:
+    Star(){
+      this->x = random(-width,width);
+      this->y = random(-height,height);
+      this->z = random(width);
+      this->pz = z;
+    }
+
+    void update(){
+      this->z -= speed;
+      if(this->z < 1.f){
+        this->z = width;
+        this->x = random(-width,width);
+        this->y = random(-height,height);
+        this->pz = z;
+      }   
+    }
+
+    void show(){
+      fill(255);
+
+      float sx = map<float>(this->x / this->z,0,1,0, width);
+      float sy = map<float>(this->y / this->z,0,1,0,height);
+
+      float r = map<float>(this->z,0,width,16,0);
+      circle(sx,sy,r);
+
+      float zx = map<float>(this->x / this->pz,0,1,0, width);
+      float zy = map<float>(this->y / this->pz,0,1,0,height);
+
+      this->pz = z;
+
+      stroke(255);
+      line(zx,zy,sx,sy);
+    }
+
+};
+
+std::vector<Star> stars;
+
+void setup(){
+    size(640,480);
+    for(int a = 0;a < 100;a++)
+      stars.push_back(Star());
 }
 
-void setup() {
-    size(540, 540);
-}
+void draw(){
+  speed = map<float>(mouseX,0,width,0.f,50.f);
+  background(0);
+  translate(width/2,height/2);
 
-void draw() {
-    background(255);
-    if(showGrid) drawGrid();
-    strokeCap(ROUND);
-    strokeWeight(1.5 * u);
+  for(int a = 0;a < stars.size();a++){
+    stars[a].update();
+    stars[a].show();
+  }
 
-    stroke(5,100,255);
-    bezier(4 * u,1 * u,7 * u,1 * u,7 * u,5 * u,4 * u,5 * u);
-
-    stroke(30,50,170);
-    line(1 * u,6 * u,4 * u,2 * u);
-
-    stroke(130,175,255);
-    line(1 * u,3 * u,2 * u,5 * u);
 }
--- sketch.cpp
+++ sketch.cpp
@@ -1,132 +1,65 @@
+//#define USE_KEY
 #include <processing.h>
 
-class Particle{
-public:
+float speed;
 
-    int life;
-    PVector pos,vel,acc;
-    PVector p;
-    color c;
-    bool q;
-    int x = 0;
-
-    Particle(){}
-
-    Particle(float x, float y,color _c,bool star){
-        this->pos = createVector(x,y);
-        if(!star)
-            this->vel = createVector(0,-8);
-        else{
-            this->vel = PVector::random2D();
-            this->vel.mult(random(2,8));
-        }
-        this->acc = createVector(0,0);
-        this->c = _c;
-        this->q = star;
-        this->life = 255;
+class Star{
+  private:
+    float x,y,z,pz;
+  public:
+    Star(){
+      this->x = random(-width,width);
+      this->y = random(-height,height);
+      this->z = random(width);
+      this->pz = z;
     }
 
-    void update(PVector f){
-        p = pos;
-        acc.add(f);
-        vel.add(acc);
-        pos.add(vel);
-        acc.mult(0);
-        life -= 5;
+    void update(){
+      this->z -= speed;
+      if(this->z < 1.f){
+        this->z = width;
+        this->x = random(-width,width);
+        this->y = random(-height,height);
+        this->pz = z;
+      }   
     }
 
-    void draw(){
-        stroke(this->c.r,this->c.g,this->c.b,this->life);
-        strokeWeight(this->q ? 4 : 8);
-        point(this->pos.x,this->pos.y);
+    void show(){
+      fill(255);
 
-        stroke(this->c.r,this->c.g,this->c.b,this->life/2);
-        strokeWeight(this->q ? 4 : 8);
-        point(p.x,p.y);
-    }
+      float sx = map<float>(this->x / this->z,0,1,0, width);
+      float sy = map<float>(this->y / this->z,0,1,0,height);
 
-    bool done(){
-        if(this->life < 0)
-            return true;
-        return false;
+      float r = map<float>(this->z,0,width,16,0);
+      circle(sx,sy,r);
+
+      float zx = map<float>(this->x / this->pz,0,1,0, width);
+      float zy = map<float>(this->y / this->pz,0,1,0,height);
+
+      this->pz = z;
+
+      stroke(255);
+      line(zx,zy,sx,sy);
     }
 
 };
 
-class Firework{
-public:
-    std::vector<Particle*> particles;
-    Particle *p;
-    color c;
-    int size;
-    bool explosed = false;
-
-    explicit Firework(int s){
-        this->size = s;
-        this->p = new Particle(random(width),height,color::rand(),false);
-        this->c = color::rand();
-    }
-
-    void update(PVector f){
-        if(!this->explosed){
-            this->p->update(f);
-            if(this->p->vel.y >= 0){
-                this->explosed = true;
-                for(int a = 0;a < this->size;a++){
-                    particles.push_back(new Particle(this->p->pos.x,this->p->pos.y,this->c,true));
-                }
-            }
-        }
-        for(int i = particles.size() - 1;i >= 0;i--){
-            particles[i]->update(f);
-        }
-    }
-
-    void draw(){
-        if(!this->explosed)
-            this->p->draw();
-        for(int i = particles.size() - 1;i >= 0;i--){
-            particles[i]->draw();
-            if(particles[i]->done()){
-                particles[i] = particles[particles.size() - 1];
-                particles.erase(particles.end() - 1);
-            }
-        }
-    }
-
-    bool done(){
-        if(this->explosed && this->particles.empty())
-            return true;
-        return false;
-    }
-};
-
-std::vector<Firework*> p;
-PVector gravity;
+std::vector<Star> stars;
 
 void setup(){
     size(640,480);
-    //fullscreen();
-    gravity = createVector(0,0.05f);
+    for(int a = 0;a < 100;a++)
+      stars.push_back(Star());
 }
 
 void draw(){
-    background(0);
-    int particles = 0;
-    if(random(1.f) < 0.2f)
-        p.push_back(new Firework(random(15,150)));
+  speed = map<float>(mouseX,0,width,0.f,50.f);
+  background(0);
+  translate(width/2,height/2);
 
-    for(int i = p.size() - 1;i >= 0;i--){
-        p[i]->update(gravity);
-        p[i]->draw();
-        if(p[i]->done()){
-            p[i] = p[p.size() - 1];
-            p.erase(p.end() - 1);
-        }else{
-            particles += p[i]->size;
-        }
-    }
-
-    printf("%i : %i\n",(int)p.size(),particles);
+  for(int a = 0;a < stars.size();a++){
+    stars[a].update();
+    stars[a].show();
+  }
 
 }
