552869758f42ea81120c03a0ea260217c61da395
maks <makuta.maks.mmc@gmail.com>
2022-01-21 22:49:19 +0200

    [1.3.7] add bezier curve drawing and fix stroke and fill

--- lib/PGL.h
+++ lib/PGL.h
@@ -21,15 +21,17 @@ void applyColor(bool x){
 // Draws an arc in the display window
 void arc(float x,float y,float w,float h,float b,float e){
     applyColor(fillFlag);
-    glBegin(GL_TRIANGLE_STRIP);
+    glBegin(fillFlag ? GL_TRIANGLE_STRIP: GL_LINE_LOOP);
     for(float a = b; a <= e+0.1f;a+=0.1f){
-        glVertex2f(x,y);
         float _x = x + w * sin(a);
         float _y = y + h * cos(a);
         glVertex2f(_x,_y);
+        if(fillFlag){
         _x = x + w * sin(a+0.05f);
         _y = y + h * cos(a+0.05f);
         glVertex2f(_x,_y);
+        glVertex2f(x,y);
+        }
     }
     glEnd();
 }
@@ -65,7 +67,7 @@ void point(float x,float y){
 // A quad is a quadrilateral, a four sided polygon
 void quad(float x1,float y1,float x2,float y2,float x3,float y3,float x4,float y4){
     applyColor(fillFlag);
-    glBegin(GL_POLYGON);
+    glBegin(fillFlag ? GL_POLYGON : GL_LINE_LOOP);
     glVertex2f(x1,y1);
     glVertex2f(x2,y2);
     glVertex2f(x3,y3);
@@ -76,14 +78,12 @@ void quad(float x1,float y1,float x2,float y2,float x3,float y3,float x4,float y
 // Draws a rectangle to the screen
 void rect(float x,float y,float w,float h){
     applyColor(fillFlag);
-    glBegin(GL_TRIANGLES);
+    glBegin(fillFlag ? GL_POLYGON : GL_LINE_LOOP);
+    glVertex2f(x,y+h);
     glVertex2f(x,y);
     glVertex2f(x+w,y);
-    glVertex2f(x,y+h);
-
-    glVertex2f(x+w,y);
-    glVertex2f(x,y+h);
     glVertex2f(x+w,y+h);
+
     glEnd();
 }
 
@@ -96,11 +96,55 @@ void square(float x,float y,float r){
 // A triangle is a plane created by connecting three points
 void triangle(float x1,float y1,float x2,float y2,float x3,float y3){
     applyColor(fillFlag);
-    glBegin(GL_TRIANGLES);
+    glBegin(fillFlag ? GL_TRIANGLES : GL_LINE_LOOP);
     glVertex2f(x1,y1);
     glVertex2f(x2,y2);
     glVertex2f(x3,y3);
     glEnd();
 }
+
+//Sets the resolution at which Beziers display
+void bezierDetail(){}
+
+//Evaluates the Bezier at point t for points a, b, c, d
+float bezierPoint(float a,float b,float c,float d,float t){
+    return  (powf((1.0 - t),3) * a) +
+            (3.0 * t * pow((1.0 - t),2)*b) +
+            (3.0 * t * t * (1.0 - t) *c) +
+            (t*t*t*d);
+}
+
+//Calculates the tangent of a point on a Bezier curve
+void bezierTangent(){}
+
+//Draws a Bezier curve on the screen
+void bezier(float x1,float y1,float x2,float y2,float x3,float y3,float x4,float y4){
+
+    applyColor(fillFlag);
+    glBegin(GL_LINE_STRIP);
+    for(float t = 0.0;t < 1.0f+0.01f;t += 0.01f){
+        float _x1 = bezierPoint(x1,x2,x3,x4,t);
+        float _y1 = bezierPoint(y1,y2,y3,y4,t);
+        glVertex2f(_x1,_y1);
+    }
+    glEnd();
+}
+
+//Sets the resolution at which curves display
+void curveDetail(){}
+
+//Evaluates the curve at point t for points a, b, c, d
+void curvePoint(){
+
+}
+
+//Calculates the tangent of a point on a curve
+void curveTangent(){}
+
+//Modifies the quality of forms created with curve() and curveVertex()
+void curveTightness(){}
+
+//Draws a curved line on the screen
+void curve(){}
 
 #endif
--- sketch.cpp
+++ sketch.cpp
@@ -1,13 +1,20 @@
 #include <processing.h>
 
 void setup() {
-  size(500, 460);
+  size(500, 500);
 }
 
 void draw() {
-  background(100);
+  background(0);
+  stroke(255,0,255);
+  bezier(100,100,200,200,500,0,500,500);
+
   fill(255);
-  rect(0,0,100,100);
-  fill(200,0,0);
-  circle(100,100,50);
+  circle(100,100,5);
+  fill(255);
+  circle(200,200,5);
+  fill(255);
+  circle(500,0,5);
+  fill(255);
+  circle(500,500,5);
 }
