03f1924e6d82f10c2ee2352cb5baa105c34f70da
maks <makuta.maks.mmc@gmail.com>
2022-01-24 20:59:47 +0200

    [1.3.9] PMath fix

--- lib/PMath.h
+++ lib/PMath.h
@@ -1,23 +1,23 @@
 #ifndef MATH_H
 #define MATH_H
 
+#include <random>
 
-    #define PI          3.1415926
+    #define PI          3.14159265358979323846
     #define HALF_PI     PI/2.0
     #define QUARTER_PI  PI/4.0
     #define TWO_PI      PI*2.0
-    #define CAPPA       0.5522847493f
+    #define TAU         TWO_PI
 
     #define max(a,b) a > b ? a : b
     #define min(a,b) a < b ? a : b
 
-    //template<class T> T rand(T min, T max){
-    //    #include <random>
-    //    std::random_device rd;
-    //    std::mt19937 gen(rd());
-    //    std::uniform_real_distribution<> distr(min, max);
-    //    return static_cast<T>(distr(gen));
-    //}
+    template<class T> T rand(T min, T max){
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_real_distribution<> distr(min, max);
+        return static_cast<T>(distr(gen));
+    }
 
     template<class T> T map(T val,T rmin,T rmax,T min,T max){
             T OldRange = rmax - rmin;
@@ -35,8 +35,8 @@
         return map<T>(val,min,max,0.f,1.f);
     }
 
-    //template<class T> T random(T x)       { return rand<T>(0,x); }
-    //template<class T> T random(T x,T y)   { return rand<T>(x,y); }
+    template<class T> T random(T x)       { return rand<T>(0,x); }
+    template<class T> T random(T x,T y)   { return rand<T>(x,y); }
 
     template<class T> T dist(T x1,T y1,T x2,T y2){
         T x = (x2 - x1) * (x2 - x1);
