a38eb3c91fee6f549503a83af8ec0b88fe7db3b8
maks <makuta.maks.mmc@gmail.com>
2022-03-07 12:32:22 +0200

    [1.3.21] update files

--- sketch.cpp
+++ sketch.cpp
@@ -7,9 +7,9 @@ void setup() {
 void draw() {
   background(0);
   float q = height/2;
-  fill(255,255,0);
-  rect(0,0,width,q);
   fill(0,0,255);
+  rect(0,0,width,q);
+  fill(255,255,0);
   rect(0,q,width,q);
 }
 
